ba2a3b170348d88a4ab8fb62f479c7fc74c70b1b
Simon Richards <s.richards@dewynters.com>
2017-03-10 14:39:51 +0000

    Js pattern

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,21 @@
-jQuery(function() {
+//Js Revealing module pattern
+var core = function($) {
 
+    var init = function() {
+    	//List functions here
+    	functionExample();
+    };
 
+	var functionExample = function() {
+
+	};
+
+    return {
+    	init: init
+    };
+
+} (jQuery);
+
+jQuery(function() { 
+	core.init(); 
 });
