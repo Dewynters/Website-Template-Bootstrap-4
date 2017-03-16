a53f43412aa14357107a2bfbfda0aa8932eb3200
Simon Richards <s.richards@dewynters.com>
2017-03-16 11:55:32 +0000

    data.php template

---
+++ build/inc/data.php
@@ -0,0 +1,72 @@
+<?php
+//Cast and creatives
+
+$cast = array( 
+	array(
+		'castName'		=> "Cast name",
+		'castRole'		=> "Cast Role",
+		'castPhoto'		=> "http://placehold.it/150x150",
+		'castBio'		=> "
+		"
+	),
+	array(
+		'castName'		=> "Cast name",
+		'castRole'		=> "Cast Role",
+		'castPhoto'		=> "http://placehold.it/150x150",
+		'castBio'		=> "
+		"
+	)
+	
+);
+
+
+$creatives = array( 
+	array(
+		'castName'		=> "Cast name",
+		'castRole'		=> "Cast Role",
+		'castPhoto'		=> "http://placehold.it/150x150",
+		'castBio'		=> "
+		"
+	),	
+	array(
+		'castName'		=> "Cast name",
+		'castRole'		=> "Cast Role",
+		'castPhoto'		=> "http://placehold.it/150x150",
+		'castBio'		=> "
+		"
+	),
+
+);
+
+
+//Gallery
+$gallery = array(
+	"Gallery 1" => array(
+		array(
+			"photo" => "http://placehold.it/900x400?text=Image 1",
+			"caption" => "Caption name"
+		),			
+		array(
+			"photo" => "http://placehold.it/900x400?text=Image 2",
+			"caption" => "Caption name"
+		),			
+		array(
+			"photo" => "http://placehold.it/900x400?text=Image 3",
+			"caption" => "Caption name"
+		),			
+	),
+	"Gallery 2" => array(
+		array(
+			"photo" => "http://placehold.it/900x400?text=Gal 2 Image 1",
+			"caption" => "Caption name"
+		),			
+		array(
+			"photo" => "http://placehold.it/900x400?text=Gal 2 Image 2",
+			"caption" => "Caption name"
+		),			
+		array(
+			"photo" => "http://placehold.it/900x400?text=Gal 2 Image 3",
+			"caption" => "Caption name"
+		),						
+	),
+);
