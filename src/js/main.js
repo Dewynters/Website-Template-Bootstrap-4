1d2339185fd0e8f86d267c4cc26f805656779841
Simon Richards <s.richards@dewynters.com>
2017-06-02 15:07:34 +0100

    Tracking

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,21 @@ var core = function($) {
 
     var init = function() {
     	//List functions here
-    	functionExample();
+    	initTrackClick();
     };
 
-	var functionExample = function() {
-
+	var initTrackClick = function() {
+        $('[data-track]').on('click', function(event) {
+            // Check if google analytics loaded
+            if(window.ga && ga.create) {
+                // Send click event to google analytics
+                ga('send', 'event', {
+                    eventCategory: $(this).data('track'),
+                    eventAction: 'click',
+                    eventLabel: $(this).attr('href')
+                });
+            }
+        });
 	};
 
     return {
